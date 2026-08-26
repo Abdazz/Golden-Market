dd4922a9fd97e9b09b4c86f26763f221d0b3bec9
Abdazz <abdoulazizzorom@gmail.com>
2026-08-26 00:58:10 +0000

    Restyle les listes d'articles du panier en cartes

--- apps/storefront/src/modules/cart/templates/preview.tsx
+++ apps/storefront/src/modules/cart/templates/preview.tsx
@@ -2,7 +2,7 @@
 
 import repeat from "@lib/util/repeat"
 import { HttpTypes } from "@medusajs/types"
-import { Table, clx } from "@modules/common/components/ui"
+import { clx } from "@modules/common/components/ui"
 
 import Item from "@modules/cart/components/item"
 import SkeletonLineItem from "@modules/skeletons/components/skeleton-line-item"
@@ -18,32 +18,23 @@ const ItemsPreviewTemplate = ({ cart }: ItemsTemplateProps) => {
   return (
     <div
       className={clx({
-        "pl-[1px] overflow-y-scroll overflow-x-hidden no-scrollbar max-h-[420px]":
-          hasOverflow,
+        "overflow-y-scroll overflow-x-hidden no-scrollbar max-h-[320px]": hasOverflow,
       })}
+      data-testid="items-table"
     >
-      <Table>
-        <Table.Body data-testid="items-table">
-          {items
-            ? items
-                .sort((a, b) => {
-                  return (a.created_at ?? "") > (b.created_at ?? "") ? -1 : 1
-                })
-                .map((item) => {
-                  return (
-                    <Item
-                      key={item.id}
-                      item={item}
-                      type="preview"
-                      currencyCode={cart.currency_code}
-                    />
-                  )
-                })
-            : repeat(5).map((i) => {
-                return <SkeletonLineItem key={i} />
-              })}
-        </Table.Body>
-      </Table>
+      {items
+        ? items
+            .sort((a, b) => {
+              return (a.created_at ?? "") > (b.created_at ?? "") ? -1 : 1
+            })
+            .map((item) => {
+              return (
+                <Item key={item.id} item={item} type="preview" currencyCode={cart.currency_code} />
+              )
+            })
+        : repeat(5).map((i) => {
+            return <SkeletonLineItem key={i} />
+          })}
     </div>
   )
 }
